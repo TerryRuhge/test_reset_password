db061e8028a6d1cd9522bf4362efe3e81e0314eb
Terry Ruhge <terry.ruhge@gmail.com>
2022-09-23 03:50:03 -0500

    Google Authentification Added to requests

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,7 +1,21 @@
 # frozen_string_literal: true
 
 class Member < ApplicationRecord
-  validates :username, presence: true
-  validates :password, presence: true
-  validates :emergency_contact_id, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2] 
+  #validates :username, presence: true
+  #validates :password, presence: true
+  #validates :emergency_contact_id, presence: true
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |member|
+      member.email = auth.info.email
+      member.password = Devise.friendly_token[0, 20]
+      member.full_name = auth.info.name 
+      member.avatar_url = auth.info.image
+    end
+  end
 end
