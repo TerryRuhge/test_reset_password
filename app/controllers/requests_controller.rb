c9b514ef14fc3d2fde8ee8f009d4847d28363606
murbina7631 <u.miguel.j@gmail.com>
2022-09-26 21:27:51 -0500

    Fixing A Change I Made
    Apparently rewording this broke the site.

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -62,7 +62,7 @@ class RequestsController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_request
-    @request = Request.find(params[:request_id])
+    @request = Request.find(params[:id])
   end
 
   # Only allow a list of trusted parameters through.
