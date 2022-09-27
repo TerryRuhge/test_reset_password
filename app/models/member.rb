9ddf8c28ee185f7d31abbbf35e01b7fce4953697
murbina7631 <u.miguel.j@gmail.com>
2022-09-27 14:04:07 -0500

    Revert "Merge branch 'test' into miguel_add_entities"
    This reverts commit 00f7ad8817051475dc9e06dd88020dd78854220a, reversing
    changes made to 80379a1710e0c3005f8ae69d6c8571e8bc58e25b.

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Member < ApplicationRecord
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -20,5 +19,4 @@ class Member < ApplicationRecord
       member.avatar_url = auth.info.image
     end
   end
-
 end
