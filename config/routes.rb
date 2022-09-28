11c8816e7cc28dacda7e3bc7922e42750f9f9773
murbina7631 <u.miguel.j@gmail.com>
2022-09-28 13:18:15 -0500

    Removing RSpec Tests
    It seems when scaffolding while rpsec is in the gemfile, it will automatically create rspec tests. I made this its own commit to possibly look back on.

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :assignments
   root 'home#index'
+  
   get "/member/rider_info", to: "member#rider_info"
-
   devise_for :members, controllers: {
      registrations:'members/registrations',
      sessions: 'members/sessions',
      omniauth_callbacks: 'members/omniauth_callbacks'
     }
-  
 
   resources :requests do
     get 'cancel'
   end
+  
+  resources :assignments
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
