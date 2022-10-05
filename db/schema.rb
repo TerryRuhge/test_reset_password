90269d696344003b76b2fcfdc47ae007f4bcd457
Terry Ruhge <terry.ruhge@gmail.com>
2022-10-05 16:03:42 -0500

    Removed Address Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_03_141842) do
+ActiveRecord::Schema.define(version: 2022_09_27_195921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "addresses", force: :cascade do |t|
-    t.string "line1"
-    t.string "line2"
-    t.string "city"
-    t.string "state"
-    t.string "zip"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
   create_table "members", primary_key: "member_id", force: :cascade do |t|
     t.boolean "is_admin"
