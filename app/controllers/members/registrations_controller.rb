edbc99bcc695bee252b0f7968202ddfd5461ed00
Terry Ruhge <terry.ruhge@gmail.com>
2022-10-05 21:19:53 -0500

    Addded new tables to views

--- app/controllers/members/registrations_controller.rb
+++ app/controllers/members/registrations_controller.rb
@@ -49,7 +49,7 @@ class Members::RegistrationsController < Devise::RegistrationsController
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :is_male])
   end
-
+  
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
   #   super(resource)
