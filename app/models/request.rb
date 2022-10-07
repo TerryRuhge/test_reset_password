f60a6f6dc236d1a5d4d914f8aaa1943965db8d9d
murbina7631 <u.miguel.j@gmail.com>
2022-10-06 20:39:21 -0500

    Unit Testing for Validations of Request and Assignment

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,5 +3,5 @@
 class Request < ApplicationRecord
 	has_many :assignment
 	
-	validates :rider_id, :date_time, :pick_up_loc, :drop_off_loc, :num_passengers, :additional_info, presence: true
+	validates :rider_id, :date_time, :pick_up_loc, :drop_off_loc, :num_passengers, presence: true
 end
