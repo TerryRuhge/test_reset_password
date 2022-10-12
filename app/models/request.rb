56b141d6c784cf37f98a5145fa19b5fbb1443250
murbina7631 <u.miguel.j@gmail.com>
2022-10-12 15:42:09 -0500

    Adding Test Search Feature to Rider

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,6 +2,7 @@
 
 class Request < ApplicationRecord
 	has_many :assignment
+	belongs_to :rider
 	
 	validates :rider_id, :date_time, :pick_up_loc, :drop_off_loc, :num_passengers, presence: true
 end
--- app/models/rider.rb
+++ app/models/rider.rb
@@ -1,2 +1,14 @@
 class Rider < ApplicationRecord
+	def self.search(search_first_name, search_last_name, search_phone_number)
+		if search_first_name && search_last_name && search_phone_number
+			rider_exist = Rider.find_by(first_name: search_first_name, last_name: search_last_name, phone_number: search_phone_number)
+			if rider_exist
+				self.where(rider_id: rider)
+			else
+				Rider.all
+			end
+		else
+			Rider.all
+		end
+	end
 end
