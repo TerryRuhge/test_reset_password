48b5f83eb09ec778af7da8a5c3ceb12fb6e54ef6
murbina7631 <u.miguel.j@gmail.com>
2022-10-12 17:04:55 -0500

    Tied the Rider and Request Id

--- app/models/rider.rb
+++ app/models/rider.rb
@@ -1,4 +1,7 @@
 class Rider < ApplicationRecord
+	has_many :request
+
+	# search feature for riders based on first and last name
 	def self.search(search_first_name, search_last_name)
 		rider_exist = Rider.find_by(first_name: search_first_name, last_name: search_last_name)
 		self.where(rider_id: rider_exist)
