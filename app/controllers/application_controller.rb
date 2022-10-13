5dcdb53c9f61629929f0f97d3e737af8cc3759d6
Terry Ruhge <terry.ruhge@gmail.com>
2022-10-12 20:38:56 -0500

    Added redirect after signing in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@
 class ApplicationController < ActionController::Base
   # / Home -> landing when site is accessed
   def show; end
+
+  private
+    #Redirect to Edit Account if signed in without making password.
+    def authenticate_member_setup!
+      if member_signed_in?
+        if !current_member.created_password
+          redirect_to edit_member_registration_path, :alert => "Please fill out the remainder of your account."
+        end
+      end
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_28_181111) do
+ActiveRecord::Schema.define(version: 2022_10_07_000438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "line1"
+    t.string "line2"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "assignments", primary_key: "assignment_id", force: :cascade do |t|
     t.integer "request_id"
