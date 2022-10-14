be3a584d43a4c3696cff46a7709f21ffed8dc09d
Terry Ruhge <terry.ruhge@gmail.com>
2022-10-13 21:24:57 -0500

    Continuation of integration tests

--- spec/feature/registration_integration_spec.rb
+++ spec/feature/registration_integration_spec.rb
@@ -2,7 +2,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Signing in with Form', type: :feature do
-
+    # scenario 'valid inputs' do
+    #     visit new_reg
+    # end
 end
 
 RSpec.describe 'Signing in with Google', type: :feature do
