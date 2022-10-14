f2fd34b989a0a3d95c58c3e0edad8e99a786cbca
Terry Ruhge <terry.ruhge@gmail.com>
2022-10-13 21:05:33 -0500

    Begin Testing

---
+++ spec/feature/registration_integration_spec.rb
@@ -0,0 +1,10 @@
+# location: spec/feature/registration_integration_spec.rb
+require 'rails_helper'
+
+RSpec.describe 'Signing in with Form', type: :feature do
+
+end
+
+RSpec.describe 'Signing in with Google', type: :feature do
+    
+end
