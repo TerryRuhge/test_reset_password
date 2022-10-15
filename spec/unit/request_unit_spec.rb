c92b4584014105019dd5b88c6659f60fc5c493d4
murbina7631 <u.miguel.j@gmail.com>
2022-10-14 21:01:40 -0500

    Updated the Rspec Tests Appropriately

--- spec/unit/request_unit_spec.rb
+++ spec/unit/request_unit_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 
 RSpec.describe Request, type: :model do
   subject do
-    described_class.create(rider_id: 1, date_time: '2022-09-18 22:26:00', pick_up_loc: '<address>', drop_off_loc: '<address>', num_passengers: 1)
+    Rider.create(rider_id: 1, first_name: "Ricardo", last_name: "Martinez", phone_number: "2105270414", local_address: "<address>")
+    described_class.create(rider_id: 1, date_time: '2022-09-18 22:26:00', pick_up_loc: '<address>', num_passengers: 1)
   end
 
   it 'is valid with valid attributes' do
@@ -22,11 +23,6 @@ RSpec.describe Request, type: :model do
   
   it 'is not valid without a pick up location' do
     subject.pick_up_loc = nil
-    expect(subject).not_to be_valid
-  end
-  
-  it 'is not valid without a drop off location' do
-    subject.drop_off_loc = nil
     expect(subject).not_to be_valid
   end
   
