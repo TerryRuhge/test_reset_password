049dd7603a5e70b8e52cabdcd07b8b0824934e32
Terry Ruhge <terry.ruhge@gmail.com>
2022-10-17 01:35:49 -0500

    Added Unit Tests for Member

--- spec/feature/session_integration_spec.rb
+++ spec/feature/session_integration_spec.rb
@@ -1,4 +1,17 @@
-# location: spec/feature/session_integration_spec.rb
-require 'rails_helper'
+# # location: spec/feature/session_integration_spec.rb
+# require 'rails_helper'
 
-RSpec.describe 'Signing up with Form', type: :system do
+# RSpec.describe 'Signing in with Form', type: :system do
+#     before do
+#         @member1 = create :member
+#         visit new_member_registration_path
+#     end
+
+#     scenario "valid with correct credentials" do
+#         fill_in "email", with: @member1.email
+#         fill_in "password", with: @member1.password
+#         click_button "sign_in"
+
+#         expect(page).to have_text @member1.email
+#     end
+# end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,6 +39,7 @@ end
 RSpec.configure do |config|
   #Used for Devise logging in testing.
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
   config.include Devise::Test::IntegrationHelpers, type: :system
   config.include Warden::Test::Helpers
   config.include FactoryBot::Syntax::Methods
