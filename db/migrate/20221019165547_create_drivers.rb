b9f35d2eb9ed51a59d7c5aebf19befcf64438714
MajaSchermuly <56056802+MajaSchermuly@users.noreply.github.com>
2022-10-19 13:13:47 -0500

    migration for drivers complete

--- db/migrate/20221019165547_create_drivers.rb
+++ db/migrate/20221019165547_create_drivers.rb
@@ -1,6 +1,12 @@
 class CreateDrivers < ActiveRecord::Migration[6.1]
   def change
-    create_table :drivers do |t|
+    create_table :drivers, id: false do |t|
+      t.primary_key :driver_id
+      t.integer :member_id
+      t.string :phone_number
+      t.string :car_model
+      t.datetime :check_in_time
+      t.string :driver_status
 
       t.timestamps
     end
