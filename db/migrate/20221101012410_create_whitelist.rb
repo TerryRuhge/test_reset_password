3d199ac2de3afe351823a36ec5bd76058fa2cfe4
MajaSchermuly <56056802+MajaSchermuly@users.noreply.github.com>
2022-10-31 20:26:24 -0500

    whitelist migration

---
+++ db/migrate/20221101012410_create_whitelist.rb
@@ -0,0 +1,8 @@
+class CreateWhitelist < ActiveRecord::Migration[6.1]
+  def change
+    create_table :whitelists do |t|
+      t.string :email
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_19_165547) do
+ActiveRecord::Schema.define(version: 2022_11_01_012410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,12 @@ ActiveRecord::Schema.define(version: 2022_10_19_165547) do
     t.boolean "is_address_BCS"
     t.integer "num_passengers"
     t.string "additional_info"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "whitelists", force: :cascade do |t|
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
