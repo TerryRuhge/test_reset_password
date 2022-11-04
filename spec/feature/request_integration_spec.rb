a702a4ce0f2db8c71e54137a0b5fa8e2dce79a2b
murbina7631 <u.miguel.j@gmail.com>
2022-11-04 06:40:14 -0500

    Fixed Rspec Test Cases

--- spec/feature/request_integration_spec.rb
+++ spec/feature/request_integration_spec.rb
@@ -5,34 +5,16 @@ require 'rails_helper'
 
 RSpec.describe 'Creating a request', type: :feature do
   scenario 'valid inputs' do
-    visit new_request_path
-    fill_in 'Name', with: 'Ricardo'
-    fill_in 'Phone number', with: '2105270414'
-    fill_in 'Pick up loc', with: '125 Spence Str, College Station, TX 77840'
-    fill_in 'Drop off loc', with: '719 S Rosemary Dr, Bryan, TX 77802'
-    fill_in 'Num passengers', with: 3
+    visit requests_incoming_path
+    fill_in 'Name:', with: 'Ricardo'
+    fill_in 'Phone Number:', with: '2105270414'
+    fill_in 'Pick Up Location:', with: '125 Spence Str, College Station, TX 77840'
+    fill_in 'Drop Off Location:', with: '719 S Rosemary Dr, Bryan, TX 77802'
+    fill_in 'Number of Passengers:', with: 3
     click_on 'Create Request'
-    visit requests_path
+    visit requests_waiting_path
     expect(page).to have_content('Ricardo')
-    expect(page).to have_content('Unassigned')
-    expect(page).to have_content('125 Spence Str, College Station, TX 77840')
-    expect(page).to have_content('719 S Rosemary Dr, Bryan, TX 77802')
-    expect(page).to have_content('3')
-  end
-end
-
-RSpec.describe 'Showing a request', type: :feature do
-  scenario 'show inputs' do
-    visit new_request_path
-    fill_in 'Name', with: 'Ricardo'
-    fill_in 'Phone number', with: '2105270414'
-    fill_in 'Pick up loc', with: '125 Spence Str, College Station, TX 77840'
-    fill_in 'Drop off loc', with: '719 S Rosemary Dr, Bryan, TX 77802'
-    fill_in 'Num passengers', with: 3
-    click_on 'Create Request'
-    visit request_path(Request.find_by_phone_number('2105270414'))
-    expect(page).to have_content('Ricardo')
-    expect(page).to have_content('Unassigned')
+    expect(page).to have_content('2105270414')
     expect(page).to have_content('125 Spence Str, College Station, TX 77840')
     expect(page).to have_content('719 S Rosemary Dr, Bryan, TX 77802')
     expect(page).to have_content('3')
@@ -41,20 +23,20 @@ end
 
 RSpec.describe 'Updating a request', type: :feature do
   scenario 'update inputs' do
-    visit new_request_path
-    fill_in 'Name', with: 'Ricardo'
-    fill_in 'Phone number', with: '2105270414'
-    fill_in 'Pick up loc', with: '125 Spence Str, College Station, TX 77840'
-    fill_in 'Drop off loc', with: '719 S Rosemary Dr, Bryan, TX 77802'
-    fill_in 'Num passengers', with: 3
+    visit requests_incoming_path
+    fill_in 'Name:', with: 'Ricardo'
+    fill_in 'Phone Number:', with: '2105270414'
+    fill_in 'Pick Up Location:', with: '125 Spence Str, College Station, TX 77840'
+    fill_in 'Drop Off Location:', with: '719 S Rosemary Dr, Bryan, TX 77802'
+    fill_in 'Number of Passengers:', with: 3
     click_on 'Create Request'
     visit edit_request_path(Request.find_by_phone_number('2105270414'))
-    fill_in 'Pick up loc', with: '676 Lubbock St, College Station, TX 77840'
-    fill_in 'Num passengers', with: 2
+    fill_in 'Pick Up Location:', with: '676 Lubbock St, College Station, TX 77840'
+    fill_in 'Number of Passengers:', with: 2
     click_on 'Update Request'
-    visit requests_path
+    visit requests_waiting_path
     expect(page).to have_content('Ricardo')
-    expect(page).to have_content('Unassigned')
+    expect(page).to have_content('2105270414')
     expect(page).to have_content('676 Lubbock St, College Station, TX 77840')
     expect(page).to have_content('719 S Rosemary Dr, Bryan, TX 77802')
     expect(page).to have_content('2')
@@ -63,43 +45,49 @@ end
 
 RSpec.describe 'Canceling a request', type: :feature do
   scenario 'cancel inputs' do
-    visit new_request_path
-    fill_in 'Name', with: 'Ricardo'
-    fill_in 'Phone number', with: '2105270414'
-    fill_in 'Pick up loc', with: '125 Spence Str, College Station, TX 77840'
-    fill_in 'Drop off loc', with: '719 S Rosemary Dr, Bryan, TX 77802'
-    fill_in 'Num passengers', with: 3
+    visit requests_incoming_path
+    fill_in 'Name:', with: 'Ricardo'
+    fill_in 'Phone Number:', with: '2105270414'
+    fill_in 'Pick Up Location:', with: '125 Spence Str, College Station, TX 77840'
+    fill_in 'Drop Off Location:', with: '719 S Rosemary Dr, Bryan, TX 77802'
+    fill_in 'Number of Passengers:', with: 3
     click_on 'Create Request'
-    visit requests_path
+    visit requests_waiting_path
     expect(page).to have_content('Ricardo')
-    expect(page).to have_content('Unassigned')
+    expect(page).to have_content('2105270414')
     expect(page).to have_content('125 Spence Str, College Station, TX 77840')
     expect(page).to have_content('719 S Rosemary Dr, Bryan, TX 77802')
     expect(page).to have_content('3')
-    click_link 'Cancel'
-    expect(page).not_to have_content('Unassigned')
+    click_on 'Cancel'
+	visit assignments_done_path
+    expect(page).to have_content('Cancelled')
+    expect(page).to have_content('Ricardo')
+    expect(page).to have_content('2105270414')
+    expect(page).to have_content('125 Spence Str, College Station, TX 77840')
+    expect(page).to have_content('719 S Rosemary Dr, Bryan, TX 77802')
     expect(page).to have_content('Cancelled')
   end
 end
 
 RSpec.describe 'Deleting a request', type: :feature do
   scenario 'delete inputs' do
-    visit new_request_path
-    fill_in 'Name', with: 'Ricardo'
-    fill_in 'Phone number', with: '2105270414'
-    fill_in 'Pick up loc', with: '125 Spence Str, College Station, TX 77840'
-    fill_in 'Drop off loc', with: '719 S Rosemary Dr, Bryan, TX 77802'
-    fill_in 'Num passengers', with: 3
+    visit requests_incoming_path
+    fill_in 'Name:', with: 'Ricardo'
+    fill_in 'Phone Number:', with: '2105270414'
+    fill_in 'Pick Up Location:', with: '125 Spence Str, College Station, TX 77840'
+    fill_in 'Drop Off Location:', with: '719 S Rosemary Dr, Bryan, TX 77802'
+    fill_in 'Number of Passengers:', with: 3
     click_on 'Create Request'
-    visit requests_path
+    visit requests_waiting_path
     expect(page).to have_content('Ricardo')
-    expect(page).to have_content('Unassigned')
+    expect(page).to have_content('2105270414')
     expect(page).to have_content('125 Spence Str, College Station, TX 77840')
     expect(page).to have_content('719 S Rosemary Dr, Bryan, TX 77802')
     expect(page).to have_content('3')
-    click_link 'Destroy'
+	visit requests_path
+    click_on 'Destroy'
     expect(page).not_to have_content('Ricardo')
-    expect(page).not_to have_content('Unassigned')
+    expect(page).not_to have_content('2105270414')
     expect(page).not_to have_content('125 Spence Str, College Station, TX 77840')
     expect(page).not_to have_content('719 S Rosemary Dr, Bryan, TX 77802')
     expect(page).not_to have_content('3')
--- spec/unit/assignment_unit_spec.rb
+++ spec/unit/assignment_unit_spec.rb
@@ -6,7 +6,7 @@ require 'rails_helper'
 RSpec.describe Assignment, type: :model do
   subject do
     Request.create(request_id: 1, name: 'Ricardo', phone_number: '2105270414', pick_up_loc: '125 Spence Str, College Station, TX 77840', drop_off_loc: '719 S Rosemary Dr, Bryan, TX 77802', num_passengers: 1)
-    described_class.create(request_id: 1, driver_id: 1)
+    described_class.create(request_id: 1, car_id: 1)
   end
 
   it 'is valid with valid attributes' do
@@ -18,8 +18,8 @@ RSpec.describe Assignment, type: :model do
     expect(subject).not_to be_valid
   end
 
-  it 'is not valid without a driver' do
-    subject.driver_id = nil
+  it 'is not valid without a car' do
+    subject.car_id = nil
     expect(subject).not_to be_valid
   end
 end
