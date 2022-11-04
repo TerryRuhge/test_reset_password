61b2c95efcc492506c10cb19f465ac5fe03ae636
Terry Ruhge <terry.ruhge@gmail.com>
2022-11-03 21:09:54 -0500

    Added Optional fields to edit registration

--- db/migrate/20220919024350_create_members.rb
+++ db/migrate/20220919024350_create_members.rb
@@ -17,26 +17,26 @@ class CreateMembers < ActiveRecord::Migration[6.1]
       #Emergency Contact
       t.string :emergency_1_full_name
       t.string :emergency_1_phone_number
-      t.string :emergency_2_full_name
-      t.string :emergency_2_phone_number
+      t.string :emergency_2_full_name, optional: true
+      t.string :emergency_2_phone_number, optional: true
       #Driver Required Information
       t.string :drivers_license_number
       t.string :state_issue_lciense
       t.boolean :current_license, default: false
       t.date :date_of_birth
       #Parent Information
-      t.string :parent_number
-      t.string :parent_street_address
-      t.string :parent_city
-      t.string :parent_state
-      t.string :parent_zip
+      t.string :parent_number, optional: true
+      t.string :parent_street_address, optional: true
+      t.string :parent_city, optional: true
+      t.string :parent_state, optional: true
+      t.string :parent_zip, optional: true
       #Miscellaneous
       t.string :tshirt_size, default: "Medium"
       t.boolean :has_insurance, default: false
-      t.string :allergies
-      t.string :special_dietary_needs
+      t.string :allergies, optional: true
+      t.string :special_dietary_needs, optional: true
       t.numeric :semesters_as_member, default: 0
-      t.string :college_major
+      t.string :college_major, optional: true
       
       t.timestamps
     end
