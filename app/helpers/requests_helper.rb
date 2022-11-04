6c6c2cfa673d3cbde33362d77cd56accdf54cd62
murbina7631 <u.miguel.j@gmail.com>
2022-11-04 06:01:31 -0500

    Bug Fix

--- app/helpers/requests_helper.rb
+++ app/helpers/requests_helper.rb
@@ -18,4 +18,10 @@ module RequestsHelper
   def time_diff(request, assignment)
     ((assignment.created_at.to_datetime - request.created_at.to_datetime) * 24 * 60).to_i.to_s + 'm'
   end
+
+  # finds the time between creation of request and cancellation (in minutes)
+  # this needs to be updated with a new request attribute to store time cancelled
+  def time_cancelled(request)
+    ((request.updated_at.to_datetime - request.created_at.to_datetime) * 24 * 60).to_i.to_s + 'm'
+  end
 end
