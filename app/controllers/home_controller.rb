c533de2fb820aa85cc648bd3722b3c7060cee7e0
Terry Ruhge <terry.ruhge@gmail.com>
2022-11-14 23:07:31 -0600

    Added basic documentation page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  before_action :authenticate_member_setup!, except: %i[new create]
+  before_action :authenticate_member_setup!, except: %i[new create doc]
   def index; end
+
+  def doc; end
 end
